c6ae5bbb693f285fe149f3bc6242339cf8caa279
TEJAS D P <108880538+TEJASDP02@users.noreply.github.com>
2023-12-13 03:52:01 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Test deploy";
+        return "Test deploy new";
     }
 
 }
